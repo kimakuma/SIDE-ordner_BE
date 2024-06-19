7382bdec85f27c158fdea2faa43769b66aabe5b6
김현아 <hyun@Hyun-MacBook-Pro.local>
2024-06-19 11:10:52 +0900

    예약내역 API

---
+++ components/reserve/reserve-model.js
@@ -0,0 +1,19 @@
+import { Logger } from '../../lib/logger/logger.js';
+import { db } from '../../lib/mysql/connect.js'
+import { search as esSearch } from '../../lib/elasticsearch/client.js';
+import { indexConfig, filterConfig, sortConfig } from "./config/index-config.js"
+
+const logger = Logger(import.meta.url);
+
+export async function list(params) {
+  const query = `
+    SELECT *
+    FROM reserveList
+    WHERE
+      userId = ${params.userId}
+  `;
+
+  const [rows] = await db.query(query);
+
+  return rows;
+};
