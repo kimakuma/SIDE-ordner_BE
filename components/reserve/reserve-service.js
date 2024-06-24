d204487525dc5c5c1e788e56f1d57571f6c3d1ee
김현아 <hyun@Hyun-MacBook-Pro.local>
2024-06-24 10:03:09 +0900

    예약내역 수정

--- components/reserve/reserve-service.js
+++ components/reserve/reserve-service.js
@@ -2,21 +2,35 @@ import * as reserveModel from './reserve-model.js';
 import { resultConfig } from "./config/index-config.js"
 
 export async function list(params) {
+  const now = new Date();
   const response = {
     status: 200,
     message: "Success",
-    results: []
+    results: {
+      "before": [],
+      "ing": [],
+      "after": []
+    }
   };
 
   const result = await reserveModel.list(params);
 
   if (result.length > 0) {
     result.map((data) => {
-      response.results.push(resultConfig("list", data))
+      const start = data.startDate;
+      const end = data.endDate;
+
+      if (now < start && now < end) {
+        response.results.before.push(resultConfig("list", data));
+      } else if (start <= now && now <= end) {
+        response.results.ing.push(resultConfig("list", data));
+      } else if (end < now) {
+        response.results.after.push(resultConfig("list", data));
+      }
     });
   } else {
     response.status = 400;
-    response.message = "예약 내역이 없슴~"
+    response.message = "None Data"
   }
 
   return response
